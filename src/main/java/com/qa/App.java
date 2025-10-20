6be150db62c9dc44139464ae4682f6085b9dc087
Ilia Jamasb <ijamasb03@gmail.com>
2025-10-20 12:25:13 +0100

    CREATE: new hello statement

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,8 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Young Ildog the Third"));
+
         System.out.println(sayGoodbye());
     }
 
